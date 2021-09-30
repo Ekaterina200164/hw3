5120352446e482fa3802cd310b0b52141f367306
Ekaterina200164 <ekaterina200164@gmail.com>
2021-09-30 20:31:07 +0300

    6

---
+++ 6.py
@@ -0,0 +1,16 @@
+def foo():
+    print('1..')
+    foo2()
+ 
+def foo2():
+    print('2..')
+    foo3()
+ 
+def foo3():
+    print('3..')
+    foo4()
+ 
+def foo4():
+    raise Exception('!!!Ошибка!!!')
+ 
+foo()
